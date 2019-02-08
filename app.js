77b6229a1da131eb0c2fd3dbc6f848b82fa6be27
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-08 15:56:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function cameraStart() {
 function cameratrigger2(){
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 1);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
     // track.stop();
