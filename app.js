6fc94f2736aadecc13b2a10b8713a09fe9e721ea
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-08 18:17:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: true , audio: false };
+var constraints = { video: { facingmode: "environment" } , audio: false };
 var track = null;
 
 // Define constants
