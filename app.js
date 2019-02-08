cb0044f0b901a68a843a6a62d043cc7078769c2f
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-08 15:51:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -34,16 +34,3 @@ function cameratrigger2(){
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
-
-
-// Install ServiceWorker
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register( '/camera-app/sw.js' , { scope : ' ' } ).then(function() {
-    console.log('CLIENT: service worker registration complete.');
-  }, function() {
-    console.log('CLIENT: service worker registration failure.');
-  });
-} else {
-  console.log('CLIENT: service worker is not supported.');
-}
