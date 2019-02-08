a44d6bd115e1b999d87822ea2b91b863f23b8e2f
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-08 16:37:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const fetch = require('node-fetch');
 const FormData = require('form-data');
 const fs = require('fs');
 
-let image_path = document.getElementById("cameraView").innerHTML;
+let image_path = "image/webp";
 let body = new FormData();
 body.append('upload', fs.createReadStream(image_path));
 
