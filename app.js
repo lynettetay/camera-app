d93cedb672896b9ac632a7bc68a5da960ddf6b2d
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-08 13:13:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function cameraStart() {
         });
 }
 
-setInterval(function () {document.getElementById("cameraTrigger").click();}, 1000);
+setInterval(function () {document.getElementById("camera--trigger").click();}, 1000);
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
