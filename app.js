79848152e47a74dbf1e9244344b8615cb8525b3f
lynettetay <47438750+lynettetay@users.noreply.github.com>
2019-02-11 14:12:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function cameratrigger2(){
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     //cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.src = cameraSensor.toDataURL("/storage/emulated/0/DCIM/aaa.jpg");
+    cameraOutput.src = cameraSensor.toDataURL("file:///storage/emulated/0/DCIM/aaa.jpg");
     cameraOutput.classList.add("taken");
     // track.stop();
 }
